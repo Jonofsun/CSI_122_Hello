b179735280c6a5bb6bbe154c6e5c99f56d6b2500
Tenshin <114845631+Jonofsun@users.noreply.github.com>
2023-01-03 18:51:38 -0800

    Initial commit

---
+++ CSI_122_Hello/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI_122_Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/* Jonathan Reed
+          * 1/3/23
+          * Hello again
+          */
+
+            Console.WriteLine("Yo, Wasgood!");
+
+        } // Main
+    } // Class
+} // namesapce
